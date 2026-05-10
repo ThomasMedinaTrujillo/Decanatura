53edfbd14ec4a9eac123bfcf86edfe76e053c3be
viegosabroso <Thommmmmmas@hotmail.com>
2026-05-09 19:08:09 -0500

    Banner fixed

--- decanatura/src/Components/Momento1Banner.tsx
+++ decanatura/src/Components/Momento1Banner.tsx
@@ -3,7 +3,7 @@ import bannerImage from '../assets/7ddfc786a7bcdcf5c85e53cfd9054089dc9a0cd3.png'
 export default function Momento1Banner() {
   return (
     <div 
-      className="w-full h-[232px] relative overflow-hidden mb-16"
+      className="w-full h-100 relative  mb-16"
       style={{
         backgroundImage: `url(${bannerImage})`,
         backgroundSize: 'cover',
@@ -16,10 +16,10 @@ export default function Momento1Banner() {
       {/* Contenido de texto */}
       <div className="relative h-full flex flex-col items-center justify-center px-[242px]">
         <div className="text-center">
-          <p className="font-bold text-[52px] text-white leading-none font-['Plus_Jakarta_Sans']">
+          <p style={{ fontFamily: 'Plus Jakarta Sans, sans-serif' }} className="font-bold text-[52px] text-white leading-none font-['Plus_Jakarta_Sans']">
             Momento 1
           </p>
-          <p className="font-bold text-[36px] text-[#e4eb60] leading-none font-['Plus_Jakarta_Sans']">
+          <p  style={{ fontFamily: 'Plus Jakarta Sans, sans-serif' }} className="font-bold text-[36px] text-[#e4eb60] leading-none font-['Plus_Jakarta_Sans']">
             Exploración
           </p>
         </div>
