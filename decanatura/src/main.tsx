ec4614b222222980f939014d7308379224c4a96d
viegosabroso <Thommmmmmas@hotmail.com>
2026-05-12 23:41:08 -0500

    Fix: 404 error finally

--- decanatura/src/main.tsx
+++ decanatura/src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.tsx'
 import Momento1 from './pages/Momento1.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createHashRouter, RouterProvider } from 'react-router'
 import Navbar from './Components/Navbar.tsx'
 import Momento2 from './pages/Momento2.tsx'
 import Momento3 from './pages/Momento3.tsx'
 
-const router = createBrowserRouter([{
+const router = createHashRouter([{
   path: '/',
   Component: Navbar,
   children:[{
